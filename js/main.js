cf46fe76fda2c890f036f46387bc48321ac96cad
aminuldev <aminuldev1@gmail.com>
2016-12-30 10:32:38 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// for wow js 
+$(function(){
+	new WOW().init();
+});
+
+
+// for mixitups
+
+$(function(){
+  $('#portfolio').mixitup({
+    targetSelector: '.item',
+    transitionSpeed: 450
+  });
+});
